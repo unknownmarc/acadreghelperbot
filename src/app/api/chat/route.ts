ea31dcf3efd6561e16ec478033852c3958bec401
Marc Stephen <marcstephen.stp@gmail.com>
2025-04-18 17:07:54 +0530

    foxed

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,18 +3,21 @@ import { google } from '@ai-sdk/google';
 import  { readFileSync } from 'fs';
 
 
+
 export async function POST(req: Request) 
 {
   const { messages }: { messages: CoreMessage[] } = await req.json();
-  console.log(message)
-
+  
+  const lastMessage = messages[messages.length - 1].content;
+  
   
     
 
     
     const  response  =  streamText({
       model: google('gemini-2.5-pro-exp-03-25'),
-      system: 'Read through the provided content and understand the information. then answer the question to the point',
+      system: `Read through the provided content and understand the information. Keep your answers short but dont miss out on key information, format your replies into proper points. Dont say "based on the provided document", just directly answer the question`,
+      
       messages:[{
         role:"user",
       
@@ -24,12 +27,18 @@ export async function POST(req: Request)
           data: readFileSync('C:/Users/marcs/acadregbot/FFCS_Academic_Regulation_4_0_Final (1)-pages-deleted-pages-deleted.pdf'),
           mimeType:'application/pdf'
         },
+        { 
+          type: 'text',
+          text: typeof lastMessage === 'string' ? lastMessage : String(lastMessage)
+        }
+        
         
         
         
         
       ],
     }]});
+    
     return response.toDataStreamResponse();
 
     
